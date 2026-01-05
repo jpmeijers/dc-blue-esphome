4892cb7778c7d2ab87f80cee1de730412602eae8
Jacques.Millard <jacques@millard.za.org>
2026-01-05 16:57:45 +0200

    add esp-idf framework support

--- esphome/components/dc_blue/dc_blue.h
+++ esphome/components/dc_blue/dc_blue.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <vector>
-
+#include <atomic>
 #include "esphome/core/component.h"
 #include "esphome/core/gpio.h"
 #include "esphome/core/hal.h"
@@ -13,15 +12,36 @@ namespace esphome
 {
   namespace dc_blue
   {
+    // Protocol constants
+    namespace protocol
+    {
+      // Command bytes (first byte of frame)
+      static constexpr uint8_t CMD_DOOR_STATE = 0x2C;
+      static constexpr uint8_t CMD_LIGHT_LOCK = 0x55;
+
+      // Door state values (after masking out AC bit)
+      static constexpr uint8_t DOOR_CLOSED = 0x20;
+      static constexpr uint8_t DOOR_MOVING = 0x08;
+      static constexpr uint8_t DOOR_OPEN = 0x02;
+
+      // Door state flags
+      static constexpr uint8_t FLAG_AC_POWER = 0x04;
+
+      // Light/lock state values
+      static constexpr uint8_t LIGHT_ON = 0x13;
+      static constexpr uint8_t LIGHT_OFF = 0x15;
+      static constexpr uint8_t STRIKE_LOCK = 0x0B;
+      static constexpr uint8_t MAGNETIC_LOCK = 0x0D;
+    } // namespace protocol
 
     class DcBlueComponent : public Component
     {
 
     public:
-      SUB_BINARY_SENSOR(open)    // CONF_OPEN
-      SUB_BINARY_SENSOR(closed)  // CONF_CLOSED
-      SUB_BINARY_SENSOR(running) // CONF_RUNNING
-      SUB_BINARY_SENSOR(light)   // CONF_LIGHT
+      SUB_BINARY_SENSOR(open)     // CONF_OPEN
+      SUB_BINARY_SENSOR(closed)   // CONF_CLOSED
+      SUB_BINARY_SENSOR(running)  // CONF_RUNNING
+      SUB_BINARY_SENSOR(light)    // CONF_LIGHT
       SUB_BINARY_SENSOR(ac_power) // CONF_AC_POWER
 
       DcBlueCover *create_garage_cover_sensor()
@@ -40,9 +60,8 @@ namespace esphome
       void set_trigger_pin(InternalGPIOPin *trigger_pin) { this->trigger_pin_ = trigger_pin; }
       void set_symbol_period(int symbol_period) { this->symbol_period_ = symbol_period; }
       void set_inverted(bool inverted) { this->inverted_ = inverted; }
-      void set_trigger_period(unsigned long trigger_period) { this->trigger_period_ = trigger_period; }
-      void set_clear_period(unsigned long clear_period) { this->clear_period_ = clear_period; }
-
+      void set_trigger_period(uint32_t trigger_period) { this->trigger_period_ = trigger_period; }
+      void set_clear_period(uint32_t clear_period) { this->clear_period_ = clear_period; }
 
       void process_door_open_event();
       void process_door_closed_event();
@@ -51,14 +70,17 @@ namespace esphome
       void process_battery_event(bool battery);
       void process_light_event(bool light);
 
-      static void interrupt_handler();
       InternalGPIOPin *data_pin_{nullptr};
       InternalGPIOPin *trigger_pin_{nullptr};
       bool inverted_ = false;
 
-      uint32_t process_queue[4];
-      int process_queue_write = 0;
-      int process_queue_read = 0;
+      // Queue for passing frames from ISR to main loop
+      // Size must be power of 2 for efficient masking in ISR
+      static constexpr uint8_t QUEUE_SIZE = 4;
+      static_assert((QUEUE_SIZE & (QUEUE_SIZE - 1)) == 0, "QUEUE_SIZE must be power of 2");
+      uint32_t process_queue_[QUEUE_SIZE] = {0};
+      volatile uint8_t process_queue_write_ = 0; // Written by ISR, read by loop()
+      volatile uint8_t process_queue_read_ = 0;  // Written by loop(), read by ISR for overflow check
 
     protected:
       void process_frame(uint32_t);
@@ -67,16 +89,21 @@ namespace esphome
       DcBlueCover *garage_cover_sensor_{nullptr};
       cover::CoverOperation next_direction_{cover::COVER_OPERATION_OPENING};
 
-      int symbol_period_ = 900;             // us
-      unsigned long trigger_period_ = 1000; // ms
-      unsigned long clear_period_ = 1000;
+      int symbol_period_ = 900;        // us
+      uint32_t trigger_period_ = 1000; // ms
+      uint32_t clear_period_ = 1000;   // ms
 
-      int triggers_needed = 0;
+      std::atomic<int> triggers_needed{0}; // Modified by DcBlueCover, read by process_trigger()
 
-      bool pin_set = false;
-      unsigned long pin_set_time;
-      unsigned long pin_cleared_time;
-      bool pin_cleared = false;
+      // Trigger pulse state machine
+      enum class TriggerState : uint8_t
+      {
+        IDLE,
+        PIN_HIGH,
+        PIN_LOW_WAIT
+      };
+      TriggerState trigger_state_ = TriggerState::IDLE;
+      uint32_t trigger_state_time_ = 0;
     };
 
   } // namespace dc_blue
